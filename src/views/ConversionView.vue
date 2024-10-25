3f7e08be3382ed7ebbb36ad69707c7621a7dc9f5
Daniel Birch-Smith <dlbs@outlook.com>
2024-10-25 17:51:20 +0200

    feat: improve conversion view to show newly converted line, support kml

--- src/views/ConversionView.vue
+++ src/views/ConversionView.vue
@@ -3,8 +3,8 @@
 
   <p>Upload GPX file</p>
 
-  <input type="file" accept=".gpx" />
-  <button @click="onConvert">Convert</button>
+  gpx: <input type="file" accept=".gpx, .kml" /> <button @click="onConvert">Convert</button>
+  {{ fileType }}
   <br />
   <input type="text" v-model="geoJsonStringVersion" />
   <button @click="copy()">Copy Output</button>
@@ -17,17 +17,24 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, type Ref } from 'vue'
-import { gpx } from '@tmcw/togeojson'
+import { computed, onMounted, ref, watch, type Ref } from 'vue'
+import { gpx, kml } from '@tmcw/togeojson'
 import { useClipboard } from '@vueuse/core'
+import { fitBounds, getMap } from '@/functions/map'
+import { center, featureCollection } from '@turf/turf'
+import type { GeoJSONSource } from 'mapbox-gl'
+import type { FeatureCollection } from 'geojson'
 
 const gpxText: Ref<string> = ref('')
+const fileType: Ref<string> = ref('')
 
 // Read the input file when the convert button is clicked
 
 function onConvert() {
   const file = document.querySelector('input[type="file"]') as HTMLInputElement
   const gpx = file.files?.[0]
+  // get the file type
+  if (gpx) fileType.value = gpx.type
   console.log('gpx:', gpx)
   if (!gpx) return
   const reader = new FileReader()
@@ -39,13 +46,53 @@ function onConvert() {
   reader.readAsText(gpx)
 }
 
+onMounted(() => {
+  const map = getMap()
+  if (!map) return
+  if (!map.getSource('conversion')) {
+    map.addSource('conversion', {
+      type: 'geojson',
+      data: featureCollection([])
+    })
+  }
+  if (!map.getLayer('conversion')) {
+    map.addLayer({
+      id: 'conversion',
+      type: 'line',
+      source: 'conversion',
+      paint: {
+        'line-color': 'rgb(110, 25, 25)',
+        'line-width': 8
+      }
+    })
+  }
+})
+
 const geoJsonVersion = computed(() => {
   try {
-    const geoJSON = gpx(new DOMParser().parseFromString(gpxText.value, 'text/xml'))
-    return geoJSON
+    let geojson
+    if (fileType.value == 'application/vnd.google-earth.kml+xml') {
+      geojson = kml(new DOMParser().parseFromString(gpxText.value, 'text/xml'))
+    } else {
+      geojson = gpx(new DOMParser().parseFromString(gpxText.value, 'text/xml'))
+    }
+    return geojson
   } catch (e) {
     console.warn('gpx parse error', e)
     return ''
+  }
+})
+
+watch(geoJsonVersion, (geojson) => {
+  const map = getMap()
+  if (!map) return
+  const source = map.getSource('conversion') as GeoJSONSource
+  if (source && geojson) {
+    source.setData(geojson as FeatureCollection)
+    const cnt = center(geojson as FeatureCollection)
+    if (cnt) {
+      map.panTo(cnt.geometry.coordinates as [number, number])
+    }
   }
 })
 
