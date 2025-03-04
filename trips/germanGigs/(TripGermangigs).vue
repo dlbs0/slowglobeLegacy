005b0549e45eb05597759d64253d30c628ceb475
Daniel Birch-Smith <dlbs@outlook.com>
2025-03-04 16:32:52 +0100

    fix: build error duplicate identifier

--- trips/germanGigs/(TripGermangigs).vue
+++ trips/germanGigs/(TripGermangigs).vue
@@ -2,7 +2,6 @@
 import DetailView from '@/components/DetailView.vue'
 import SGHeader from '@/components/SGHeader.vue'
 import SGGallery from '@/components/SGGallery.vue'
-import SGGallery from '@/components/SGGallery.vue'
 import SGMapCutout from '@/components/SGMapCutout.vue'
 import SGText from '@/components/SGText.vue'
 import { germanGigs } from './germanGigs'
